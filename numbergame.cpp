153b286646c9ee456f72d0ada8922e7338393192
Layne Locovare <layne@ajlmediainc.com>
2025-04-18 19:00:17 -0400

    Update and rename helloworld.cpp to numbergame.cpp

--- numbergame.cpp
+++ numbergame.cpp
@@ -14,10 +14,11 @@ int main()
 
         cout << "Guess The Number\n";
         cout << "This number I have in my head is between 1 and 100!\n";
+        cout << number << '\n';
         cout << "Your Guess: ";
         cin >> guess;
 
-        while (guess != number)
+        for (int i = 1; i < 5; i++)
         {
                 if (guess > number)
                 {
@@ -25,17 +26,32 @@ int main()
                         cout << "Guess again: ";
                         cin >> guess;
                 }
+
                 else if (guess < number)
                 {
                         cout << "The number I'm thinking of is higher than this!\n";
                         cout << "Guess again: ";
                         cin >> guess;
                 }
+
+                else if (i <= 5 && guess == number)
+                {
+                        break;
+                }
+
+                else if (guess != number)
+                {
+                        break;
+                }
         }
 
         if (guess == number)
         {
                 cout << "YOU GOT THE NUMBER!!!\n"; 
+        }
+        else
+        {
+                cout << "You didnt get the number :(\n";
         }
 
         return 0;
